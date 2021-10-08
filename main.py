b2504d3205c1af69ed6341e6961078d97f54dfbd
Mathieu Duong <mathieu.duong1@gmail.com>
2021-10-08 13:53:35 +0200

    itk test

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import numpy as np
-import itk
+import itk, os
 
+# ----------------------
+# Lecture de l'image
+# ----------------------
+image_dir = 'images'
+input_filename = os.path.join(image_dir, 'brain.png')
+image = itk.imread(input_filename)
+ImageType = type(image)
+# On récupère le type de l'image
+
+# ----------------------
+# Création et paramétrage du filtre
+# ----------------------
+
+# On créé une instance du filtre qui prend en entrée et ressort des images du même type que l'image d'entrée
+smoothFilter = itk.SmoothingRecursiveGaussianImageFilter[ImageType, ImageType].New() 
+smoothFilter.SetInput(image) # On spécifie l'image d'entrée du filtre
+
+sigma = 0.7
+smoothFilter.SetSigma(sigma) # On sélectionne la variance du filtre
+
+# ----------------------
+# Écriture de la sortie du filtre
+# ----------------------
+itk.imwrite(smoothFilter.GetOutput(), os.path.join(image_dir,'myImage_smoothed.png')) # On écrit sur le disque la sortie du filtre Gaussien
